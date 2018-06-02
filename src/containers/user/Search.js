bca54419c987614b2fc5d591e54809a36bb94736
jinsphan <pvtinh1996@gmail.com>
2018-06-02 21:10:03 +0700

    df

--- src/containers/user/Search.js
+++ src/containers/user/Search.js
@@ -1,12 +1,20 @@
 import React from "react";
 
 class Search extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    onSubmit = () => {
+        console.log("Submit");
+    }
 
     render() {
         return(
             <div>
                 <h1 className="txt-red">Hello Search {process.env.NODE_ENV}</h1>
                 <img src="assets/images/amazon.png" alt=""/>
+                <div class=''></div>
             </div>
         )
     }
